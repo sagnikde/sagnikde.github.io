8710e03931bde6b1662fbf5be9658f8949ba1dad
Sagnik De <sagnik.de@ssi-schaefer.us>
2019-03-12 16:08:02 -0400

    Changed the navigation to buttons.  Makes the job a bit easier.

---
+++ jsstuff/navigation.js
@@ -0,0 +1,8 @@
+
+
+let tabclicked = (tabname) => {
+    let tab = document.getElementById("content");
+    let contentname = tabname+"content";
+    let contentpage = document.getElementById("content");
+
+}
