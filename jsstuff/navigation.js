443a1cc261d48a65d9877fb7ad58bde98a4e5b0a
Sagnik De <sagnik.de@ssi-schaefer.us>
2019-03-18 15:41:07 -0400

    Added some dynamic behaviour

--- jsstuff/navigation.js
+++ jsstuff/navigation.js
@@ -7,5 +7,28 @@ let tabclicked = (tabname) => {
     let colour = window.getComputedStyle(tab).getPropertyValue("background-color");
     console.log(colour);
     contentpage.style.borderLeftColor=colour;
+};
 
-}
+let window_resized = () => {
+    let content = document.getElementById("content");
+    let wh = window.innerHeight;
+    let ww = window.innerWidth;
+    let navcon = document.getElementById("navandcon");
+    let navbuttons = document.getElementById("navigation");
+    if(ww<wh){
+        navcon.classList.replace('flex-row','flex-column');
+        navbuttons.classList.replace('flex-column','flex-row');
+        let buttons = document.getElementByClass("navtab");
+        for(eachbutton : buttons)
+    }else{
+        navcon.classList.replace('flex-column','flex-row');
+        navbuttons.classList.replace('flex-row','flex-column');
+        navbuttons.styles.fontSize = "12px";
+    }
+    console.log(navcon.classList);
+    console.log('resized !');
+};
+
+window.addEventListener('resize',(e)=>{window_resized();});
+
+
